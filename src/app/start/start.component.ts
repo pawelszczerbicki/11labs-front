9b164c522e615bcfe3594e940c2eb5eb2979e965
Pawel Szczerbicki <pawel.szczerbicki@arcsoftware.it>
2025-02-23 01:05:36 +0100

    custom

--- src/app/start/start.component.ts
+++ src/app/start/start.component.ts
@@ -17,15 +17,17 @@ export class StartComponent {
   surveyForm: FormGroup;
   step = 1;
   saving = false
-  lessons = ['Courage', 'Thriftiness', 'Patience', 'Diligence', 'Honesty', 'Kindness', 'Humility', 'Self-discipline', 'Perseverance', 'Empathy'];
+  lessons = ['Courage', 'Thriftiness', 'Patience', 'Diligence', 'Honesty', 'Kindness', 'Humility', 'Self-discipline', 'Perseverance', 'Empathy', 'Responsibility', 'Gratitude', 'Respect', 'Creativity', 'Teamwork', 'Generosity', 'Forgiveness', 'Optimism', 'Open-mindedness', 'Graciousness'];
   ages = Array.from({length: 11}, (_, i) => i + 5);
-  elements = ['Castles', 'Princesses', 'Dragons', 'Unicorns', 'Pirates', 'Robots', 'Dinosaurs', 'Space', 'Elves', 'Knights'];
+  elements = ['Castles', 'Princesses', 'Dragons', 'Unicorns', 'Pirates', 'Robots', 'Dinosaurs', 'Space', 'Elves', 'Knights', 'Wizards', 'Fairies', 'Treasure', 'Magic', 'Adventure', 'Heroes', 'Villains', 'Monsters', 'Aliens', 'Spacecraft']
+
 
   constructor(private fb: FormBuilder, private router: Router, private http: HttpClient, private toast: ToastrService) {
     this.surveyForm = this.fb.group({
       name: [''],
       lesson: [''],
       elements: [[]],
+      customElement: ['']
     });
   }
 
@@ -38,6 +40,8 @@ export class StartComponent {
   }
 
   save = () => {
+    if(this.surveyForm.value.customElement)
+      this.surveyForm.value.elements.push(this.surveyForm.value.customElement);
     this.saving = true
     this.http.post("/story", this.surveyForm.value).pipe(tap(() => this.toast.success("Story added")), finalize(() => this.saving = false))
       .subscribe(()=>this.router.navigate(["/parent"]));
