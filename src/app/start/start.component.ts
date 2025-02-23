8f5bab09f27cf6f69eed7bc5a3fec917ebc3e354
Pawel Szczerbicki <pawel.szczerbicki@arcsoftware.it>
2025-02-23 10:56:36 +0100

    start

--- src/app/start/start.component.ts
+++ src/app/start/start.component.ts
@@ -1,11 +1,11 @@
-import { Component } from '@angular/core';
-import { FormBuilder, FormGroup, ReactiveFormsModule } from '@angular/forms';
-import { CommonModule } from '@angular/common';
-import { Router, RouterModule } from '@angular/router';
-import { HttpClient } from '@angular/common/http';
-import { ToastrService } from 'ngx-toastr';
-import { finalize, tap } from 'rxjs';
-import { LoadingComponent } from '../shared/loading/loading.component';
+import {Component} from '@angular/core';
+import {FormBuilder, FormGroup, ReactiveFormsModule} from '@angular/forms';
+import {CommonModule} from '@angular/common';
+import {Router, RouterModule} from '@angular/router';
+import {HttpClient} from '@angular/common/http';
+import {ToastrService} from 'ngx-toastr';
+import {finalize, tap} from 'rxjs';
+import {LoadingComponent} from '../shared/loading/loading.component';
 
 @Component({
   selector: 'app-start',
@@ -19,56 +19,56 @@ export class StartComponent {
   saving = false;
 
   lessons = [
-    { name: 'Courage', emoji: '🦁' },
-    { name: 'Thriftiness', emoji: '💰' },
-    { name: 'Patience', emoji: '🧘' },
-    { name: 'Diligence', emoji: '🎯' },
-    { name: 'Honesty', emoji: '🤝' },
-    { name: 'Kindness', emoji: '💝' },
-    { name: 'Humility', emoji: '🙏' },
-    { name: 'Self-discipline', emoji: '💪' },
-    { name: 'Perseverance', emoji: '🏃' },
-    { name: 'Empathy', emoji: '❤️' },
-    { name: 'Responsibility', emoji: '✅' },
-    { name: 'Gratitude', emoji: '🙌' },
-    { name: 'Respect', emoji: '🌟' },
-    { name: 'Creativity', emoji: '🎨' },
-    { name: 'Teamwork', emoji: '👥' },
-    { name: 'Generosity', emoji: '🎁' },
-    { name: 'Forgiveness', emoji: '🕊️' },
-    { name: 'Optimism', emoji: '☀️' },
-    { name: 'Open-mindedness', emoji: '🧠' },
-    { name: 'Graciousness', emoji: '👑' },
+    {name: 'Courage', emoji: '🦁'},
+    {name: 'Thriftiness', emoji: '💰'},
+    {name: 'Patience', emoji: '🧘'},
+    {name: 'Diligence', emoji: '🎯'},
+    {name: 'Honesty', emoji: '🤝'},
+    {name: 'Kindness', emoji: '💝'},
+    {name: 'Humility', emoji: '🙏'},
+    {name: 'Self-discipline', emoji: '💪'},
+    {name: 'Perseverance', emoji: '🏃'},
+    {name: 'Empathy', emoji: '❤️'},
+    {name: 'Responsibility', emoji: '✅'},
+    {name: 'Gratitude', emoji: '🙌'},
+    {name: 'Respect', emoji: '🌟'},
+    {name: 'Creativity', emoji: '🎨'},
+    {name: 'Teamwork', emoji: '👥'},
+    {name: 'Generosity', emoji: '🎁'},
+    {name: 'Forgiveness', emoji: '🕊️'},
+    {name: 'Optimism', emoji: '☀️'},
+    {name: 'Open-mindedness', emoji: '🧠'},
+    {name: 'Graciousness', emoji: '👑'},
   ];
-  ages = Array.from({ length: 13 }, (_, i) => i + 3);
+  ages = Array.from({length: 13}, (_, i) => i + 3);
   elements = [
-    { name: 'Castles', emoji: '🏰' },
-    { name: 'Princesses', emoji: '👸' },
-    { name: 'Dragons', emoji: '🐉' },
-    { name: 'Unicorns', emoji: '🦄' },
-    { name: 'Pirates', emoji: '🏴‍☠️' },
-    { name: 'Robots', emoji: '🤖' },
-    { name: 'Dinosaurs', emoji: '🦖' },
-    { name: 'Space', emoji: '🌌' },
-    { name: 'Elves', emoji: '🧝' },
-    { name: 'Knights', emoji: '⚔️' },
-    { name: 'Wizards', emoji: '🧙' },
-    { name: 'Fairies', emoji: '🧚' },
-    { name: 'Treasure', emoji: '💎' },
-    { name: 'Magic', emoji: '✨' },
-    { name: 'Adventure', emoji: '🗺️' },
-    { name: 'Heroes', emoji: '🦸' },
-    { name: 'Villains', emoji: '😈' },
-    { name: 'Monsters', emoji: '👾' },
-    { name: 'Aliens', emoji: '👽' },
-    { name: 'Spacecraft', emoji: '🚀' },
+    {name: 'Castles', emoji: '🏰'},
+    {name: 'Princesses', emoji: '👸'},
+    {name: 'Dragons', emoji: '🐉'},
+    {name: 'Unicorns', emoji: '🦄'},
+    {name: 'Pirates', emoji: '🏴‍☠️'},
+    {name: 'Robots', emoji: '🤖'},
+    {name: 'Dinosaurs', emoji: '🦖'},
+    {name: 'Space', emoji: '🌌'},
+    {name: 'Knights', emoji: '⚔️'},
+    {name: 'Wizards', emoji: '🧙'},
+    {name: 'Fairies', emoji: '🧚'},
+    {name: 'Magic', emoji: '✨'},
+    {name: 'Adventure', emoji: '🗺️'},
+    {name: 'Heroes', emoji: '🦸'},
+    {name: 'Aliens', emoji: '👽'},
+    {name: 'Spacecraft', emoji: '🚀'},
+    {name: 'LEGO', emoji: '🧱'},
+    {name: 'Police', emoji: '🚔'},
+    {name: 'Democracy', emoji: '🏛️'},
+    {name: 'Huggy Wuggy', emoji: '🧸'},
   ];
 
   voices = [
-    { id: 'tqA6i6KHhXNOU8bqdxNY', name: 'Winnie-the-Pooh' },
-    { id: 'pQVwW8uwbObGejdyhYiQ', name: 'Rapunzel' },
-    { id: '21m00Tcm4TlvDq8ikWAM', name: 'Rachel' },
-    { id: 'nPczCjzI2devNBz1zQrb', name: 'Brian' }
+    {id: 'tqA6i6KHhXNOU8bqdxNY', name: 'Winnie-the-Pooh'},
+    {id: 'pQVwW8uwbObGejdyhYiQ', name: 'Rapunzel'},
+    {id: '21m00Tcm4TlvDq8ikWAM', name: 'Rachel'},
+    {id: 'nPczCjzI2devNBz1zQrb', name: 'Brian'}
   ];
 
   constructor(
@@ -109,7 +109,7 @@ export class StartComponent {
         elements: elements.filter((e) => e !== element),
       });
     } else {
-      this.surveyForm.patchValue({ elements: [...elements, element] });
+      this.surveyForm.patchValue({elements: [...elements, element]});
     }
   }
 }
